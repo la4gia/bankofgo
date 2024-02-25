6328c11c0133fdbbded3d7fe145d4944290e8855
Anthony LaForgia <105323071+la4gia@users.noreply.github.com>
2024-02-25 06:03:19 +0000

    add cicd file

---
+++ cicd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $(docker ps | grep $2) ]] 
+    then
+        if [[ $(docker ps | grep $3) ]] 
+            then 
+                docker container stop $3
+            else 
+                echo "container not running"
+        fi
+        docker rmi $2
+    else
+        echo "no image detected"
+fi
+cd $1 && docker build -t $2 .
+docker run -d --rm --name $3 -p $4:$4 $2
